5f895e2ae44c287e46280fe17a9cf98b26d470aa
Khalilnkh <khalilnkh@code.edu.az>
2022-11-02 00:53:39 +0400

    created

---
+++ Controllers/PricingController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebTask.Controllers
+{
+    public class PricingController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
